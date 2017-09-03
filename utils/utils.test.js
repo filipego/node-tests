6af47e20a06347ab1ccf00d1a7dd41855e59428c
Filipe Valente Gomes <Filipe@Filipes-iMac.fios-router.home>
2017-09-03 13:40:35 -0400

    added expect

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,18 +1,29 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33,11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44 but got ${res}` )
-  }
+  expect(res).toBe(44).toBeA('number');
 });
 
 
 it('should square a number', () => {
   var res = utils.square(5);
 
-  if (res !== 25) {
-    throw new Error(`Expected 25 but got ${res}` )
-  }
+  expect(res).toBe(25).toBeA('number');
+});
+
+it('should expect some values', () => {
+  expect(12).toNotBe(11);
+});
+
+it('should set first and last name', () => {
+  let user = { location: 'Newark', age: 39 };
+  let res = utils.setName(user, 'Filipe Valente');
+
+  expect(res).toInclude({
+    firstName: 'Filipe',
+    lastName: 'Valente'
+  });
 });
